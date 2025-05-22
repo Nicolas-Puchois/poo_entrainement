362997e9d7d9b9c761391252b396f064f9aaac57
Nicolas-Puchois <nicolas.puchois@laposte.net>
2025-05-22 15:57:09 +0200

    add feedback

--- main.php
+++ main.php
@@ -8,3 +8,10 @@ $tata = new Joueur("tata", "F", "druide", 15, 70, true);
 // $toto = new Joueur();
 
 $traban = new Voiture("Traban", "601", "bleu", 5, false);
+$dacia = new Voiture("Dacia", "Spring", "bleu", 5, true);
+$mini = new Voiture("Mini", "Cooper", "rouge", 5, false);
+
+
+var_dump($traban);
+var_dump($dacia);
+var_dump($mini);
